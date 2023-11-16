6a5e5da0cfde291abddaa033abc9f0a5aecfca7b
PriyanshuRanjann <103645328+PriyanshuRanjann@users.noreply.github.com>
2023-11-17 01:46:29 +0530

    Time: 39 ms (58.12%), Space: 16.5 MB (7.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,12 +1,19 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) > len(t):return False
-        if len(s) == 0:return True
-        subsequence=0
-        for i in range(0,len(t)):
-            if subsequence <= len(s) -1:
-                print(s[subsequence])
-                if s[subsequence]==t[i]:
-
-                    subsequence+=1
-        return  subsequence == len(s) 
+        s_pointer=0
+        t_pointer=0
+        while s_pointer<len(s) and t_pointer<len(t):
+            if s[s_pointer]==t[t_pointer]:
+                s_pointer+=1
+            t_pointer+=1
+        return s_pointer==len(s)
+        '''
+        new=""
+        for i in t:
+            if i in s:
+                new+=i
+        if new==s:
+            return True
+        else:
+            return False
+        '''
